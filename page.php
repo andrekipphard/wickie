fd41db4e28c514703b7950edde621cded47ba380
André Kipphard <andre.kipphard@real-digital.de>
2024-07-22 11:13:28 +0200

    starting introducing acf

--- page.php
+++ page.php
@@ -16,103 +16,17 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-		<?php get_template_part( 'template-parts/components/hero' );?>
-		<?php get_template_part( 'template-parts/components/feature-cards' );?>
-		<?php get_template_part( 'template-parts/components/credit-card' );?>
-		<?php get_template_part( 'template-parts/components/rewards' );?>
-		<?php get_template_part( 'template-parts/components/market' );?>
-		<?php get_template_part( 'template-parts/components/services' );?>
-		<?php get_template_part( 'template-parts/components/steps' );?>
-		<?php get_template_part( 'template-parts/components/content-card' );?>
-		<?php get_template_part( 'template-parts/components/feature-boxes' );?>
-		<?php get_template_part( 'template-parts/components/content-card-list' );?>
-		<?php get_template_part( 'template-parts/components/pricing' );?>
-		<?php get_template_part( 'template-parts/components/banner' );?>
-		<?php get_template_part( 'template-parts/components/testimonials' );?>
-		<?php get_template_part( 'template-parts/components/blog-posts' );?>
-		<?php get_template_part( 'template-parts/components/banner-two-buttons' );?>
-		<?php /* if (have_rows('content')):?>
+
+		<?php if (have_rows('content')):?>
 			<?php while( have_rows('content')): the_row(); 
 
 				if( get_row_layout() == 'hero' ):
 					
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
+					get_template_part( 'template-parts/components/'. get_row_layout() );
 
-				elseif( get_row_layout() == 'category_slider' ): 
+				elseif( get_row_layout() == '' ): 
 
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'community' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'sale_slider' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'test' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'lifestyle' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'categories' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'sale_products' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'category_tabs' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'contact' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'hero_single' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'products' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'headline_text' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'image_text' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'image_text_left' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'background_image_text' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'vertical_slider' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'point_of_sale' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'products_of_category' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
-
-				elseif( get_row_layout() == 'faq' ): 
-
-					get_template_part( 'template-parts/layouts/'. get_row_layout() );
+					get_template_part( 'template-parts/components/'. get_row_layout() );
 				
 				endif; 
 
@@ -133,7 +47,7 @@ get_header();
 				echo "</div>";
 	
 			endwhile; // End of the loop.
-		endif;*/
+		endif;
 		?>
 
 	</main><!-- #main -->
--- template-parts/components/hero.php
+++ template-parts/components/hero.php
@@ -1,35 +1,68 @@
+<?php
+    $headline = get_sub_field('headline');
+    $subline = get_sub_field('subline');
+    $imageTopLeft = get_sub_field('image_top_left');
+    $imageBottomLeft = get_sub_field('image_bottom_left');
+    $imageTopRight = get_sub_field('image_top_right');
+    $imageBottomRight = get_sub_field('image_bottom_left');
+    $imageCenter = get_sub_field('image_center');
+?>
 <section class="hero">
     <div class="container">
         <div class="content">
-            <h1>One app, all things money</h1>
-            <span>Open a free account in minutes right from your phone and make your money go further.</span>
-            <div class="cta">
-                <button type="button" class="btn btn-white">
-                    <i class="bi bi-apple"></i>
-                    App Store
-                </button>
-                <button type="button" class="btn btn-white">
-                    <i class="bi bi-google-play"></i>
-                    Google Play
-                </button>
-            </div>
+            <h1><?= $headline; ?></h1>
+            <span><?= $subline; ?></span>
+            <?php if( have_rows('cta')):?>
+                <div class="cta">
+                    <?php while( have_rows('cta') ): the_row();
+                    $buttonUrl = get_sub_field('button_url');
+                    $buttonIcon = get_sub_field('button_icon');
+                    $buttonText = get_sub_field('button_text');?>
+                        <a href="<?=$buttonUrl;?>">
+                            <button type="button" class="btn btn-white">
+                                <i class="bi bi-<?= $buttonIcon; ?>"></i>
+                                <?= $buttonText; ?>
+                            </button>
+                        </a>
+                    <?php endwhile;?>
+                </div>
+            <?php endif;?>
         </div>
         <div class="images">
             <div class="top-left">
-                <img loading="lazy" decoding="async" src="<?php echo get_template_directory_uri(); ?>/assets/images/hero/home3-5.jpg">
+                <img loading="lazy" decoding="async" src="<?= wp_get_attachment_image_url($imageTopLeft, 'large');?>">
             </div>
             <div class="bottom-left">
-                <img loading="lazy" decoding="async" src="<?php echo get_template_directory_uri(); ?>/assets/images/hero/home3-2.jpg">
+                <img loading="lazy" decoding="async" src="<?= wp_get_attachment_image_url($imageBottomLeft, 'large');?>">
             </div>
             <div class="top-right">
-                <img loading="lazy" decoding="async" src="<?php echo get_template_directory_uri(); ?>/assets/images/hero/home3-4.jpg">
+                <img loading="lazy" decoding="async" src="<?= wp_get_attachment_image_url($imageTopRight, 'large');?>">
             </div>
             <div class="bottom-right">
-                <img loading="lazy" decoding="async" src="<?php echo get_template_directory_uri(); ?>/assets/images/hero/home3-3.jpg">
+                <img loading="lazy" decoding="async" src="<?= wp_get_attachment_image_url($imageBottomRight, 'large');?>">
             </div>
             <div class="center">
-                <img loading="lazy" decoding="async" src="<?php echo get_template_directory_uri(); ?>/assets/images/hero/home3-1.jpg">
+                <img loading="lazy" decoding="async" src="<?= wp_get_attachment_image_url($imageCenter, 'large');?>">
             </div>
         </div>
     </div>
 </section>
+
+<?php if( have_rows('tab')):?>
+    <div class="accordion accordion-flush mt-3" id="accordionFlushExample">
+    <?php while( have_rows('tab') ): the_row();
+        $titel = get_sub_field('titel');
+        $text = get_sub_field('text');?>
+        <div class="accordion-item mb-1">
+            <h2 class="accordion-header">
+                <button style="background-color:#e0dbd5; font-weight:500;" class="accordion-button collapsed text-primary" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?= get_row_index();?>" aria-expanded="false" aria-controls="flush-collapse<?= get_row_index();?>">
+                    <?= $titel;?>
+                </button>
+            </h2>
+            <div style="background-color:#f1ede9;" id="flush-collapse<?= get_row_index();?>" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body"><?= $text;?></div>
+            </div>
+        </div>
+    <?php endwhile;?>
+    </div>
+<?php endif;?>
