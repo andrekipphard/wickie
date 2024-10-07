5a945c263d7db9658d9b7c33d57a06ecb6436713
André Kipphard <andre.kipphard@real-digital.de>
2024-10-07 16:06:12 +0200

    credit card content responsiv

--- template-parts/components/credit_card_content.php
+++ template-parts/components/credit_card_content.php
@@ -51,21 +51,8 @@
                     <?php endwhile;?>
                 </div>
             <?php endif;?>
-        </div>
-        <div class="credit-card-info">
-            <h2><?= $headline; ?></h2>
-            <span><?= $subline; ?></span>
-            <?php if( have_rows('list_item')):?>
-                <ul>
-                    <?php while( have_rows('list_item') ): the_row();
-                    $list_item_icon = get_sub_field('list_item_icon');
-                    $list_item_text = get_sub_field('list_item_text');?>
-                        <li><i class="bi bi-<?= $list_item_icon; ?>"></i><span><?= $list_item_text; ?></span></li>
-                    <?php endwhile;?>
-                </ul>
-            <?php endif;?>
             <?php if( have_rows('button')):?>
-                <div class="cta">
+                <div class="cta hide-desktop">
                     <?php while( have_rows('button') ): the_row();
                     $buttonStyle = get_sub_field('button_style');
                     $buttonText = get_sub_field('button_text');
@@ -89,5 +76,44 @@
                 </div>
             <?php endif;?>
         </div>
+
+        <div class="credit-card-info">
+            <h2><?= $headline; ?></h2>
+            <span><?= $subline; ?></span>
+            <?php if( have_rows('list_item')):?>
+                <ul>
+                    <?php while( have_rows('list_item') ): the_row();
+                    $list_item_icon = get_sub_field('list_item_icon');
+                    $list_item_text = get_sub_field('list_item_text');?>
+                        <li><i class="bi bi-<?= $list_item_icon; ?>"></i><span><?= $list_item_text; ?></span></li>
+                    <?php endwhile;?>
+                </ul>
+            <?php endif;?>
+            <?php if( have_rows('button')):?>
+                <div class="cta hide-mobile">
+                    <?php while( have_rows('button') ): the_row();
+                    $buttonStyle = get_sub_field('button_style');
+                    $buttonText = get_sub_field('button_text');
+                    $buttonUrl = get_sub_field('button_url');
+                    $buttonIcon = get_sub_field('button_icon');?>
+                        <a href="<?=$buttonUrl;?>">
+                            <?php if($buttonStyle == 'Link'):?>
+                                <button type="button" class="btn btn-link">
+                                    <?= $buttonText; ?>
+                                    <i class="bi bi-chevron-right"></i>
+                                </button>
+                            <?php else:?>
+                                <button type="button" class="btn btn-primary">
+                                    <?php if($buttonStyle == 'Icon Left'):?><i class="bi bi-<?= $buttonIcon; ?>" style="margin-right:10px;"></i><?php endif;?>
+                                    <?= $buttonText; ?>
+                                    <?php if($buttonStyle == 'Icon Right'):?><i class="bi bi-<?= $buttonIcon; ?>"></i><?php endif;?>
+                                </button>
+                            <?php endif;?>
+                        </a>
+                    <?php endwhile;?>
+                </div>
+            <?php endif;?>
+        </div>
+        
     </div>
 </section>
